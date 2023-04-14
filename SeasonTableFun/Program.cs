5e1f068b6f8c88f2a2727b877d3c9303beff08b8
GET Academy <terje@getacademy.no>
2023-04-14 09:49:53 +0200

    Initial commit

---
+++ SeasonTableFun/Program.cs
@@ -0,0 +1,8 @@
+﻿using SeasonTableFun;
+
+var teamA = new Team("Lag A");
+var teamB = new Team("Lag B");
+var seasonTable = new SeasonTable(new[] {teamA, teamB,});
+
+seasonTable.AddMatch(new Match(teamA, teamB, 3, 2));
+seasonTable.AddMatch(new Match(teamB, teamA, 0, 0));
---
+++ SeasonTableFun/SeasonTable.cs
@@ -0,0 +1,24 @@
+﻿namespace SeasonTableFun
+{
+    internal class SeasonTable
+    {
+        private readonly List<Match> _matches;
+        private readonly Team[] _teams;
+
+        public SeasonTable(Team[] teams)
+        {
+            _matches = new List<Match>();
+            _teams = teams;
+        }
+
+        public void AddMatch(Match match)
+        {
+
+        }
+
+        public void Show()
+        {
+
+        }
+    }
+}
